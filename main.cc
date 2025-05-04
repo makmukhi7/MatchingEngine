057c4d2bfc7f4041fdb4cb73e072f77c57099fcd
makmukhi <mahak.vmukhi@gmail.com>
2025-05-04 16:37:12 -0700

    Update order

--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@
 #include "messages.h"
 
 int main(int argc, char** argv) {
-    mukhi::matching_engine::TradeEvent te{ .price = 75, .qty = 10 };
+    mukhi::matching_engine::TradeEvent te{ .qty = 10, .price = 75 };
     std::cout << te << std::endl;
     return 0;
 }
