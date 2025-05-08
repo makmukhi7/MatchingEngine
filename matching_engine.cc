f597a413f73a6194d70b65bde85709a03bd3aed3
makmukhi <mahak.vmukhi@gmail.com>
2025-05-07 18:25:20 -0700

    minor updates

--- matching_engine.cc
+++ matching_engine.cc
@@ -5,12 +5,9 @@ namespace mukhi::matching_engine {
 void MatchingEngine::Start() {
     std::string line;
     while (std::getline(is_, line)) {
-        // std::getline(is_, line);
-        if (!line.empty()) {
-            auto req = parse(line, es_);
-            if (req.has_value()) {
-                std::visit([this](auto&& arg) {ob_.ProcessOrder(arg);}, *req);
-            }
+        auto req = parse(line, es_);
+        if (req.has_value()) {
+            std::visit([this](auto&& arg) {ob_.ProcessOrder(arg);}, *req);
         }
     }
 }
